bbe53ecc0d86bdb06a2a9bb8e3cedb2c7e9ce673
Dave <davehtx@hotmail.com>
2017-08-12 08:14:10 -0500

    feed-ui table

--- Dave Social App2/RoundButton.swift
+++ Dave Social App2/RoundButton.swift
@@ -28,8 +28,12 @@ class RoundButton: UIButton {
     override func layoutSubviews() {
         super.layoutSubviews()
         
+//        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+//        layer.shadowOpacity = 0.0
+//        layer.shadowRadius = 5.0
+//        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+    
         layer.cornerRadius = self.frame.width / 2
-        
     }
     
     
