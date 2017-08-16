6237b679c2fa32f1662d199555ca19adaf454d57
Dave <davehtx@hotmail.com>
2017-08-15 21:42:33 -0500

    completed getting firebase display

--- Dave Social App2/FeedVC.swift
+++ Dave Social App2/FeedVC.swift
@@ -75,10 +75,17 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let post = posts[indexPath.row]
-        print("DAVE: Post caption \(post.caption)")
-        
 
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            cell.configureCell(post: post)
+            print("DAVE: Post caption \(post.caption)")
+            return cell
+        } else {
+            return PostCell()
+            
+        }
+
+       
         
     }
     
